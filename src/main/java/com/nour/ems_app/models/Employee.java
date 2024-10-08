0658641bbe7be17ba0b893bb753443e7c4a6b2e3
nour <174273417+NourDoesCode@users.noreply.github.com>
2024-10-08 10:08:14 +0100

    addd employee entity

---
+++ src/main/java/com/nour/ems_app/models/Employee.java
@@ -0,0 +1,29 @@
+package com.nour.ems_app.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Entity
+@Table(name = "employees")
+public class Employee {
+    @Id
+    @GeneratedValue
+    private Long id;
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+    @Column(unique = true, nullable = false)
+    private String email;
+}
